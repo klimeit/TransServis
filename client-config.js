a6448c4b7a01d3ab647df5d781a3563e3041c2dd
klimeit <goshachebotok@gmail.com>
2026-08-01 14:26:32 +0300

    Update client-config.js

--- client-config.js
+++ client-config.js
@@ -1,4 +1,4 @@
 window.CLIENT_SOURCE = {
-  url: "https://script.google.com/macros/s/ВАШ_КОД/exec",
+  url: "https://script.google.com/macros/s/AKfycbzhjRkPWbdvA3gMlRjbj5jAbYZodeUywC212IRm36KZ6-manaFh_VmIZnft7ezgLTXm/exec",
   pub: "tst-client-2026"
 };
